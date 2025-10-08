8f711b7c1095cd703b86a998b64f7d17366321cc
fbabaei <fbmails@gmail.com>
2025-10-08 06:05:04 +0000

    Fix: app.py for streamlit permission

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+import os
+os.environ["STREAMLIT_HOME"] = os.path.join(os.getcwd(), ".streamlit")
+
 import streamlit as st
 import tensorflow as tf
 import numpy as np
 import pickle
 
-# Load model and scaler
+# Load model & scaler
 model = tf.keras.models.load_model("model/model.keras")
 with open("model/scaler.pkl", "rb") as f:
     scaler = pickle.load(f)
 
 st.title("🏥 Health Risk Predictor")
 
+# Inputs
 age = st.number_input("Age", min_value=0, max_value=120, value=45)
 bmi = st.number_input("BMI", min_value=10.0, max_value=60.0, value=25.0)
 bp = st.number_input("Blood Pressure", min_value=60, max_value=180, value=120)
